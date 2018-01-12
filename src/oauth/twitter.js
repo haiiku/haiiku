916b0e9f574855c7a7d1400d4f6bc2d35ed96168
VEBER Arnaud <arnaud@veber.pw>
2018-01-12 18:14:57 +0100

    feat(oauth): add twitter user function

--- src/oauth/twitter.js
+++ src/oauth/twitter.js
@@ -70,3 +70,38 @@ module.exports.authToken = (event, context, callback) => {
     });
   });
 };
+
+module.exports.userData = (event, context, callback) => {
+  const body = JSON.parse(event.body);
+
+  const endpoint = 'https://api.twitter.com/1.1/account/verify_credentials.json';
+  const access_token = body.access_token;
+  const access_secret = body.access_secret;
+
+  // get user data
+  oauth.get(endpoint, access_token, access_secret, (error, data) => {
+    // fail
+    if(error) {
+      console.error(error);
+      return callback(new Error('[500] Internal Server Error'));
+    }
+
+    // prepare payload
+    data = JSON.parse(data);
+    var payload = {
+      id: data.id,
+      name: data.name,
+      screen_name: data.screen_name,
+      profile_url: 'https://twitter.com/' + data.screen_name,
+    };
+
+    // send response
+    return callback(null, {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+      body: JSON.stringify(payload)
+    });
+  });
+};
