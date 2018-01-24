ab1b2a0dd7ebfb8ffb5d303a4a1087fe8fae2cfd
VEBER Arnaud <arnaud@veber.pw>
2018-01-24 17:15:39 +0100

    feat(lambda): get comments function

--- src/comments.js
+++ src/comments.js
@@ -3,6 +3,43 @@
 const AWS = require('aws-sdk');
 const dynamo = new AWS.DynamoDB.DocumentClient();
 
+module.exports.get = (event, context, callback) => {
+  // dynamo db query params
+  const params = {
+    TableName: process.env.TABLE_NAME,
+    IndexName: process.env.POST_INDEX_NAME,
+    ConsistentRead: false,
+    KeyConditionExpression: '#sid = :sid and #aid = :aid',
+    ExpressionAttributeNames: {
+      '#sid': 'site_id',
+      '#aid': 'post_id',
+    },
+    ExpressionAttributeValues: {
+      ':sid': process.env.SITE_ID,
+      ':aid': event.pathParameters.post_id,
+    },
+    Limit: process.env.QUERY_LIMIT,
+  };
+
+  // dynamo db query
+  dynamo.query(params, (error, data) => {
+    if (error) {
+      console.error(error);
+      return callback(new Error('[500] Internal Server Error'));
+    }
+
+    return callback(null, {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+      body: JSON.stringify({
+        data: data.Items,
+      }),
+    });
+  });
+};
+
 module.exports.put = (event, context, callback) => {
   // get timestamp
   const timestamp = new Date().getTime();
